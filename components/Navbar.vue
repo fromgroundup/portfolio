3e35e128f2c297489cb887e2885cdf2b36d773b0
fromgroundup <fromgroundup@protonmail.com>
2021-05-14 02:02:36 +1000

    Added basic page, component and layout files

---
+++ components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="navbar">
+        <div><nuxt-link to="/">Home</nuxt-link></div>
+        <div><nuxt-link to="/portfolio">Portfolio</nuxt-link></div>
+        <div><nuxt-link to="/blog">Blog</nuxt-link></div>
+        <div><nuxt-link to="/contact">Contact</nuxt-link></div>
+    </div>
+</template>
+
+<style>
+.navbar {
+    display: flex;
+    justify-content: space-evenly;
+}
+</style>
