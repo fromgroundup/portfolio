c28ae3fad9dfc268d0016699492800b997d8e29b
fromgroundup <fromgroundup@protonmail.com>
2021-05-15 01:33:46 +1000

    Implemented post loading via asyncData

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,3 +1,15 @@
 <template>
-    <div></div>
+    <div>{{posts}}</div>
 </template>
+
+<script>
+export default {
+    async asyncData({ $content, params }) {
+        const posts = await $content('posts').only(['title', 'updatedAt', 'slug', 'subtitle']).fetch()
+
+        return {
+            posts
+        }
+    }
+}
+</script>
---
+++ pages/post/_slug/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{article}}</div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $content, params }) {
+        const article = await $content('posts', params.slug).fetch();
+        return {
+            article
+        }
+    }
+}
+</script>
