869041348644e07414528f16a19c08302b231f22
fromgroundup <fromgroundup@protonmail.com>
2021-05-18 00:09:14 +1000

    Added rendering elements to blog post pages

--- pages/post/_slug/index.vue
+++ pages/post/_slug/index.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>{{article}}</div>
+    <div class="post">
+        <div class="heading">{{post.title}}</div>
+        <div class="heading">{{post.subtitle}}</div>
+        <nuxt-content :document="post" class="content"/>
+    </div>
 </template>
 
 <script>
 export default {
     async asyncData({ $content, params }) {
-        const article = await $content('posts', params.slug).fetch();
+        const post = await $content('posts', params.slug).fetch();
         return {
-            article
+            post
         }
     }
 }
