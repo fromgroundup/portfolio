f310a1e3ad2ff16d246563317b33a4392ad54f58
fromgroundup <fromgroundup@protonmail.com>
2021-05-18 02:52:41 +1000

    Created components for projects and skills

---
+++ components/ProjectListing.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="project-listing">
+        <div>{{project.name}}</div>
+        <div>{{project.summary}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['project']
+}
+</script>
---
+++ components/SkillListing.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="skill-listing">
+        <div>{{skill.name}}</div>
+        <div>{{skill.summary}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['skill']
+}
+</script>
